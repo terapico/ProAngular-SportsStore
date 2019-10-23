3caa13003e21c1cdd27948dc3c03b0f940a1c7e1
Jeffrey Sze <terapico@gmail.com>
2019-10-23 14:45:28 -0700

    Added customDirective. End of Chapter 7.

--- src/app/store/store.module.ts
+++ src/app/store/store.module.ts
@@ -1,3 +1,4 @@
+import { CounterDirective } from './counter.directive';
 import { StoreComponent } from './store.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { ModelModule } from './../model/model.module';
@@ -7,7 +8,7 @@ import { FormsModule } from "@angular/forms";
 
 @NgModule({
     imports: [ModelModule, BrowserModule, FormsModule],
-    declarations: [StoreComponent],
+    declarations: [StoreComponent, CounterDirective],
     exports: [StoreComponent]
 })
 export class StoreModule { }
